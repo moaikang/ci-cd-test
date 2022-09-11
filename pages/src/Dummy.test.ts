b45b04b5726acf40923d0e3f54172de4c5536ce9
moaikang <moaikang.dev@gmail.com>
2022-09-11 20:01:05 +0900

    Pull Request CI

---
+++ pages/src/Dummy.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, it } from "@jest/globals";
+
+describe("더미 테스트", () => {
+  it("1 + 1 = 귀요미", () => {
+    expect("귀요미").toEqual("귀요미");
+  });
+
+  it("2 + 2 = 귀요미", () => {
+    expect("귀요미").toEqual("귀요미");
+  });
+
+  it("3 + 3 = 귀요미", () => {
+    expect("귀요미").toEqual("귀요미");
+  });
+
+  it("4 + 4 = 귀요미", () => {
+    expect("귀요미").toEqual("귀요미");
+  });
+});
